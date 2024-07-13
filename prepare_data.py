f9e6d6d4c894d44af8a1b247c5d2f78f19e09a3d
Nicolas Murphy <murphynick10@gmail.com>
2024-07-12 21:06:28 -0500

    remove html tags

--- prepare_data.py
+++ prepare_data.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import re
+
+def clean_joke(joke):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', joke)
 
 jokes_df = pd.read_csv("jokes.csv", header=None)
 jokes = jokes_df.iloc[:, 0].tolist()
 
+cleaned_jokes = [clean_joke(joke) for joke in jokes]
+
 with open("jokes.txt", "w", encoding="utf-8", errors="ignore") as f:
-    for joke in jokes:
+    for joke in cleaned_jokes:
         f.write(joke + "\n")
